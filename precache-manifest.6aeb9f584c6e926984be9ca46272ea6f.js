8fc5d9e4b7a03e7b2e2375a2e9ec0ba506497a37
Ronakk07 <ronakkhangaonkar13@gmail.com>
2021-04-30 20:16:20 +0530

    Updates

---
+++ precache-manifest.6aeb9f584c6e926984be9ca46272ea6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1e076eef61a2272bdb090fc9837942f",
+    "url": "/my-codepen/index.html"
+  },
+  {
+    "revision": "3c24d4b6dd9ce18af69c",
+    "url": "/my-codepen/static/css/2.76184554.chunk.css"
+  },
+  {
+    "revision": "1bca301543c0325d2a21",
+    "url": "/my-codepen/static/css/main.724e6fe1.chunk.css"
+  },
+  {
+    "revision": "3c24d4b6dd9ce18af69c",
+    "url": "/my-codepen/static/js/2.b9b97afe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-codepen/static/js/2.b9b97afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bca301543c0325d2a21",
+    "url": "/my-codepen/static/js/main.1ea67124.chunk.js"
+  },
+  {
+    "revision": "13f21261a0b97f9ce0b5",
+    "url": "/my-codepen/static/js/runtime-main.85a1922e.js"
+  }
+]);
